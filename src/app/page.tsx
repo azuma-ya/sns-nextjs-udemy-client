0773f77dd09ca0595749e4b3ee9a67610ba6d581
azuma-ya <kazu.bluemountain@gmail.com>
2023-05-11 23:05:44 +0900

    feat: #1 投稿機能を実装する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from "@/components/Navbar";
+import Timeline from "@/components/Timeline";
 
 export default function Home() {
 	return (
 		<div>
-			<Navbar />
+			<Timeline />
 		</div>
 	);
 }
